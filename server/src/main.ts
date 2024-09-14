97a8a8b9b991c911c55e3eb1bb17029f5f9fd386
Kauã Santos <kaua.santos2@estudante.ifms.edu.br>
2024-09-14 07:42:01 -0400

    fazer em casa

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,8 @@ import { basename } from "path";
 import { log } from "console";
 import { prependOnceListener } from "process";
 import InserirProduto from "./inserir-produtos";
+import ListaUsuarios from "./lista-usuarios";
+import InserirUsuarios from "./inserir-usuarios";
 
 const app = Express()
 app.use(Cors())
@@ -41,9 +43,42 @@ app.post("/produtos",async(req,res)=>{
         }
     }
 })
+app.get("/usuarios",async(req,res)=>{
+    const listaUsuarios = new ListaUsuarios()
+    res.send(await listaUsuarios.execute())
+})
+
+app.post("/usuarios",async(req,res)=>{
+    console.log("Alguém tentou cadastrar usuarios");
+    console.log(req.body.id)
+    const{id, nome, idade, cpf, rg, endereco, estado_civil} = req.body
+    const usuarios =  {
+        id,
+        nome,
+        idade,
+        cpf,
+        rg,
+        endereco,
+        estado_civil
+    }
+    const inserirUsuarios = new InserirUsuarios()
+    try{
+        const usuariosInserido = inserirUsuarios.execute(usuarios)
+        res.status(201).send(usuariosInserido)
+    }
+    catch(e:any){
+        if(e.code === 'ER_DUP_ENTRY'){
+            console.log("Erro: Chave Primária Duplicada")
+            res.status(400).send("Usuario já cadastrado")
+        }else{
+            console.log(e)
+            res.status(409).send("Erro Desconhecido: Olhe o TERMINAL do VCODE")
+        }
+    }
+})
 
 const porta = 8000
-app.listen(porta,()=>{
-    console.log("Server Rodando")
-    console.log("digite: localhost:8000/produtos na url para acessar o servidor.")
-})
+app.listen(porta, () => {
+    console.log(`Server Rodando na porta ${porta}`);
+    console.log("Acesse: localhost:8000/produtos ou /usuarios para acessar o servidor.");
+});
